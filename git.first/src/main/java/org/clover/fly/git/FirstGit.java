b996d5f26b50c304e6da30bd37f449acf96978ab
clover-yy <brent_yu@hotmail.com>
2011-07-11 15:41:40 +0800

    Git first.

---
+++ git.first/src/main/java/org/clover/fly/git/FirstGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.clover.fly.git;
+
+/**
+ * @author yuuuvyan
+ *
+ */
+public class FirstGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi, Git");
+	}
+
+}
